ad9fa36f237035d24f2a1b3ad5c6fdfab436ecc0
Marc Schreiber <schrieveslaach@online.de>
2017-01-29 12:10:52 +0100

    Formatting and first template usage

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,1 +1,4 @@
-<h1>Hello world</h1>
+<center>
+    <h1 id="app-heading"><?php p($l->t('SPG Verein')); ?></h1>
+    <img id="logo" src="<?php print_unescaped(image_path('spgverein', 'app_gray.svg')); ?>">
+</center>
--- templates/index.php
+++ templates/index.php
@@ -4,15 +4,15 @@ style('spgverein', 'style');
 ?>
 
 <div id="app">
-	<div id="app-navigation">
-		<?php print_unescaped($this->inc('navigation/index')); ?>
-		<?php print_unescaped($this->inc('settings/index')); ?>
-	</div>
+    <div id="app-navigation">
+        <?php print_unescaped($this->inc('navigation/index')); ?>
+        <?php print_unescaped($this->inc('settings/index')); ?>
+    </div>
 
-	<div id="app-content">
-		<div id="app-content-wrapper">
-			<?php print_unescaped($this->inc('content/index')); ?>
-		</div>
-	</div>
+    <div id="app-content">
+        <div id="app-content-wrapper">
+            <?php print_unescaped($this->inc('content/index')); ?>
+        </div>
+    </div>
 </div>
 
