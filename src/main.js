fdc160c39fd81747b90f2c07954633063afd8599
Marc Schreiber <info@schrieveslaach.de>
2020-06-26 06:37:37 +0200

    Load german locale for dayjs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './app.vue';
 import store from './store.js';
 import dayjs from 'dayjs';
+import 'dayjs/locale/de';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 
+dayjs.locale('de');
 dayjs.extend(localizedFormat);
 
 export default new Vue({
